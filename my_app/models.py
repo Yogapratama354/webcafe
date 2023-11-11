ef7e49eb263ca108389d9092014203d91bb08a82
Yogapratama354 <yogadwipratama75@gmail.com>
2023-11-11 13:50:20 +0700

    first

---
+++ my_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Home(models.Model):
+    nama = models.CharField(max_length=100)
+    keterangan = models.TextField()
+
+    def __str__ (self):
+        return f"{self.nama}"
+
+class Menu(models.Model):
+    nama = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nama}"
---
+++ my_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('my_app/', views.my_app, name='my_app'),
+    path('menu/', views.menu, name='menu'),
+]
